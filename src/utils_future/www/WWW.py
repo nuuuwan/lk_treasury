6340d5a41403475c4cd2287e709ba9e1d56e9e56
nuuuwan <nuuuwan@gmail.com>
2025-09-26 08:20:40 +0530

    [push_to_clients] Updated utils_future

--- src/utils_future/www/WWW.py
+++ src/utils_future/www/WWW.py
@@ -1,50 +1,24 @@
-import ssl
 from functools import cached_property
 
-import requests
 import urllib3
 from bs4 import BeautifulSoup
-from requests.adapters import HTTPAdapter
 from urllib3.exceptions import InsecureRequestWarning
 from utils import Log
+
+from utils_future.www.WWWSSLMixin import WWWSSLMixin
 
 log = Log("WebPage")
 
 urllib3.disable_warnings(InsecureRequestWarning)
 
 
-class WWWSSLMixin:
-    class SSLAdapter(HTTPAdapter):
-        def __init__(self, ssl_context: ssl.SSLContext, **kwargs):
-            self._ctx = ssl_context
-            super().__init__(**kwargs)
-
-        def init_poolmanager(
-            self, connections, maxsize, block=False, **pool_kwargs
-        ):
-            pool_kwargs["ssl_context"] = self._ctx
-            return super().init_poolmanager(
-                connections, maxsize, block, **pool_kwargs
-            )
-
-        def proxy_manager_for(self, proxy, **proxy_kwargs):
-            proxy_kwargs["ssl_context"] = self._ctx
-            return super().proxy_manager_for(proxy, **proxy_kwargs)
-
-    @staticmethod
-    def get_session() -> requests.Session:
-        ctx = ssl.create_default_context()
-        ctx.set_ciphers("DEFAULT:@SECLEVEL=1")
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-
-        s = requests.Session()
-        s.mount("https://", WWWSSLMixin.SSLAdapter(ctx))
-        return s
-
-
 class WWW(WWWSSLMixin):
     TIMEOUT = 120
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        + "AppleWebKit/537.36 (KHTML, like Gecko) "
+        + "Chrome/127.0.0.0 Safari/537.36"
+    }
 
     def __init__(self, url):
         self.url = url
@@ -64,7 +38,10 @@ class WWW(WWWSSLMixin):
     def content(self):
         try:
             response = WWW.get_session().get(
-                self.url, timeout=self.TIMEOUT, verify=False
+                self.url,
+                verify=False,
+                timeout=self.TIMEOUT,
+                headers=self.HEADERS,
             )
             response.raise_for_status()
             content = response.text
