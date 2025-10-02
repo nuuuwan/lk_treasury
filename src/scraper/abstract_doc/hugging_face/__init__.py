f5f40ebc74684ee97b0bca5e5f3bb58bf4f5ed50
nuuuwan <nuuuwan@gmail.com>
2025-10-02 17:19:51 +0530

    [push_to_clients] Deleted scraper

--- src/scraper/abstract_doc/hugging_face/__init__.py
+++
@@ -1,5 +0,0 @@
-# scraper.abstract_doc.hugging_face (auto generate by build_inits.py)
-# flake8: noqa: F408
-
-from scraper.abstract_doc.hugging_face.AbstractDocHuggingFaceMixin import \
-    AbstractDocHuggingFaceMixin
